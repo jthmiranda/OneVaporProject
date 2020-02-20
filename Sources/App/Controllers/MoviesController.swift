a02fd556cefd280e8a38140d056cc0455263f49a
Jonathan Miranda <miranda.jth@gmail.com>
2020-02-19 19:30:02 -0600

    creating method to add relations to pivot table

--- Sources/App/Controllers/MoviesController.swift
+++ Sources/App/Controllers/MoviesController.swift
@@ -12,9 +12,30 @@ struct MoviesController: RouteCollection {
     func boot(router: Router) throws {
         let moviesRouter = router.grouped("api", "movies")
         moviesRouter.post(Movies.self, at: "create", use: createMovie)
+        moviesRouter.post(MovieUser.self, at: "addRelation", use: addMovieUser)
     }
 }
 
 func createMovie(_ req: Request, movie: Movies) throws -> Future<Movies> {
     return movie.save(on: req)
+}
+
+func addMovieUser(_ req: Request, movieUser: MovieUser) throws -> Future<HTTPStatus> {
+    return flatMap(
+        Users.query(on: req)
+            .filter(\.userid == movieUser.user)
+            .first()
+            .unwrap(or: Abort(.notFound, reason: "There's no user")),
+        Movies.query(on: req)
+            .filter(\.title == movieUser.title)
+            .first()
+            .unwrap(or: Abort(.notFound, reason: "There's no movie"))) { (user, movie) in
+                let pivot = try UsersMoviesPivot(user.requireID(), movie.requireID())
+                return pivot.save(on: req).transform(to: .created)
+    }
+}
+
+struct MovieUser: Content {
+    let title: String
+    let user: String
 }
